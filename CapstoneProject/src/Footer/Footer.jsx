9f3e2dd75aa40416ca42f8f80536fd879ac4d06b
VictorOkpare <victorokpare@gmail.com>
2024-05-03 11:20:46 +0100

    FEAT: I included global stying and made adjustment to the stylings in the header and footer section FEAT: I allowed the image in the download section to overflow into the comment section and also included padding to the comment section

--- CapstoneProject/src/Footer/Footer.jsx
+++ CapstoneProject/src/Footer/Footer.jsx
@@ -1,72 +1,55 @@
-import React from 'react'
-import "./Footer.css"
-import {CiSearch} from "react-icons/ci"
-
+import "./Footer.css";
+import { CiSearch } from "react-icons/ci";
 
 function Footer() {
   return (
-    <div className='f-wapper'>
-        <div className='f-container'>
-            <div className='f-ctn'>
-                <h2 id='f-header'>Quick Lift </h2>
-                <p>Lorem ipsum dolor sit amet. Dolores<br/>
-                 ipsa earum quasi esse nobis<br/> recusandae? earum quod reprehenderit<br/>
-                 perspiciatis quas? Assumenda
-                </p>
+    <div className="f-wrapper">
+      <div className="f-container">
+        <div className="f-ctn">
+          <h2 id="f-header">Quick Lift</h2>
+          <p>
+            Lorem Ipsum hddndn idtewu ioljl <br />
+            jdjdip Lorem Ipsum hddndn idtewu <br />
+            ioljl jdjdipLorem Ipsum hddndn <br />
+            idtewu ioljl jdjdip
+          </p>
 
-                <div className='f-search'>
-                    <input type="text" />
-                    <span><CiSearch/></span>
-                </div>
-            </div>
-
-            <div className='f-ctn'>
-                <h5 className='f-head'> QuickLift</h5>
-                <p className='address'>
-                    Plot 1210 <br />
-                    water coperation drive <br />
-                    oppiste landmark events center <br />
-                    oniru <br />
-                    victoria island, Lagos.
-                </p>
-
-            </div>
-
-            <div className='f-ctn'>
-                <h5 className='f-head'>PARTNER WITH US</h5>
-                <div className='list'>
-                    <span>Fleets</span> <br />
-
-                    <span>Franchise</span> <br />
-                     
-                    <span>Influences</span> <br />
-            </div>
-
-          
-                    
-                </div>
-
-                <div className='f-ctn'>
-                    <h5 className='f-head'>COMPANY</h5>
-                    <div className="List">
-                    
-                        <span>About Use</span> <br />
-
-                        <span>Contact</span> <br />
-
-                        <span>Blog</span> <br />
-
-                        <span>Brand Guildlines</span> <br />
-                    </div>
-            </div>
-
-
-           
+          <div className="f-search">
+            <input type="text" />
+            <span><CiSearch /></span>
+          </div>
         </div>
-
-        
+        <div className="f-ctn">
+          <h5 className="f-head">Quick Lift</h5>
+          <p className="address">
+            Plot 1210, <br />
+            Water corporation drive <br />
+            Opposite landmark events centre <br />
+            Oniru, <br />
+            Victoria Island , Lagos.
+          </p>
+        </div>
+        <div className="f-ctn">
+          <h5 className="f-head">PARTNER WITH US</h5>
+          <div className="list">
+            <span>Sign up as a driver</span>
+            <span>Fleets</span>
+            <span>Franchise</span>
+            <span>Influencers</span>
+          </div>
+        </div>
+        <div className="f-ctn">
+          <h5 className="f-head">COMPANY</h5>
+          <div className="list">
+            <span>About Us</span>
+            <span>Contact</span>
+            <span>Blog</span>
+            <span>Brand Guidelines</span>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
